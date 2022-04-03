1fd389e80b629e48ac1ac0057f67523821c80053
John Murray <me@johnmurray.io>
2022-04-03 15:24:16 -0400

    shared thing

---
+++ shared/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"C"
+	"fmt"
+)
+
+// export SharedThing
+func SharedThing(msg string, times int) {
+	for i := 0; i < times; i++ {
+		fmt.Println(msg)
+	}
+}
+
+func main() {}
