2cba09e490128fbb81a4b0a5259c0d3ad2fd48b5
John Murray <me@johnmurray.io>
2022-04-13 23:27:39 -0400

    test cleanup

--- yaai/parser/test/listener.go
+++ yaai/parser/test/listener.go
@@ -1,12 +1,20 @@
 package test
 
 import (
-	"testing"
+	"fmt"
 
 	"github.com/JohnMurray/yaii/yaai/parser"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
-	"github.com/stretchr/testify/assert"
 )
+
+// Test implementationf or a parse-tree and error listener
+type testListener struct {
+	*parser.BaseYaaiListener
+	*antlr.DefaultErrorListener
+
+	packageName string
+	errors      []error
+}
 
 func createParser(input string) (*parser.YaaiParser, *testListener) {
 	// setup the input
@@ -31,10 +39,19 @@ func createParser(input string) (*parser.YaaiParser, *testListener) {
 	return p, tl
 }
 
-func TestParsePackageDecl(t *testing.T) {
-	p, tl := createParser("package Stonks\n")
-	antlr.ParseTreeWalkerDefault.Walk(tl, p.Package_decl())
+// Populate the listener with error encountered. Try and form
+// something that's somewhat readable to troubleshoot.
+func (tl *testListener) SyntaxError(recognizer antlr.Recognizer,
+	offendingSymbol interface{},
+	line, column int,
+	msg string,
+	e antlr.RecognitionException) {
+	tl.errors = append(tl.errors, fmt.Errorf(
+		"[%d:%d] (%v) %s",
+		line, column, offendingSymbol, msg,
+	))
+}
 
-	assert.Equal(t, "Stonks", tl.packageName)
-	assert.Empty(t, tl.errors)
+func (tl *testListener) ExitPackage_decl(c *parser.Package_declContext) {
+	tl.packageName = c.IDENTIFIER().GetText()
 }
