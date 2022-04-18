8f0b63186d471d38529d860a4909fbe78549847a
John Murray <me@johnmurray.io>
2022-04-18 19:23:29 -0400

    snapshot testing, but broken :-P

--- yaai/parser/test/package_test.go
+++ yaai/parser/test/package_test.go
@@ -3,41 +3,61 @@ package test
 import (
 	"testing"
 
+	"github.com/JohnMurray/yaii/yaai/parser"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParsePackageDecl(t *testing.T) {
-	testInputs := [][]string{
-		{"package Stonks\n", "Stonks"},             // title-case
-		{"package lowercase\n", "lowercase"},       // lowercase
-		{"package thing1thing2\n", "thing1thing2"}, // with numbers
-		{"package thing\r", "thing"},               // \r line-ending
-		{"package thing\n\n\r\n\r\n\n", "thing"},   // multiple line-endings
-	}
-	for _, test := range testInputs {
-		t.Run(test[0], func(t *testing.T) {
-			p, tl := createParser(test[0])
-			antlr.ParseTreeWalkerDefault.Walk(tl, p.PackageClause())
-			assert.Equal(t, test[1], tl.packageName)
-			assert.Empty(t, tl.errors)
-		})
-	}
+func TestTokenParsing(t *testing.T) {
+
+	lexer := parser.NewYaaiLexer(antlr.NewInputStream(`package myPackage
+
+type MyStruct struct {
+	thing      int
+	otherThing string
 }
 
-func TestParseBadPackageDecl(t *testing.T) {
-	testInputs := []string{
-		"package Stonks",         // no newline
-		"stuff package Stonks\n", // leading crap
-		"package 9things\n",      // can't start with number
-		"packageThing\n",         // no space
-		"Package Stonks\n",       // uppercase package
-	}
-	for _, test := range testInputs {
-		t.Run(test, func(t *testing.T) {
-			p, tl := createParser(test)
-			antlr.ParseTreeWalkerDefault.Walk(tl, p.PackageClause())
-			assert.NotEmpty(t, tl.errors)
-		})
+actor MyActor {
+	receive (x int) -> {
+		// this is a comment
+		// actor MyCommentActor {}
 	}
 }
+`))
+	assert.NoError(t, lexerSnapshot("package-simple", lexer))
+}
+
+// func TestParsePackageDecl(t *testing.T) {
+// 	testInputs := [][]string{
+// 		{"package Stonks\n", "Stonks"},             // title-case
+// 		{"package lowercase\n", "lowercase"},       // lowercase
+// 		{"package thing1thing2\n", "thing1thing2"}, // with numbers
+// 		{"package thing\r", "thing"},               // \r line-ending
+// 		{"package thing\n\n\r\n\r\n\n", "thing"},   // multiple line-endings
+// 	}
+// 	for _, test := range testInputs {
+// 		t.Run(test[0], func(t *testing.T) {
+// 			p, tl := createParser(test[0])
+// 			antlr.ParseTreeWalkerDefault.Walk(tl, p.PackageClause())
+// 			assert.Equal(t, test[1], tl.packageName)
+// 			assert.Empty(t, tl.errors)
+// 		})
+// 	}
+// }
+//
+// func TestParseBadPackageDecl(t *testing.T) {
+// 	testInputs := []string{
+// 		"package Stonks",         // no newline
+// 		"stuff package Stonks\n", // leading crap
+// 		"package 9things\n",      // can't start with number
+// 		"packageThing\n",         // no space
+// 		"Package Stonks\n",       // uppercase package
+// 	}
+// 	for _, test := range testInputs {
+// 		t.Run(test, func(t *testing.T) {
+// 			p, tl := createParser(test)
+// 			antlr.ParseTreeWalkerDefault.Walk(tl, p.PackageClause())
+// 			assert.NotEmpty(t, tl.errors)
+// 		})
+// 	}
+// }
---
+++ yaai/parser/test/snapshot.go
@@ -0,0 +1,114 @@
+package test
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/JohnMurray/yaii/yaai/parser"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+type snapshotOpts struct {
+	generate    bool
+	snapshotDir string
+}
+
+func lexerSnapshot(name string, lexer *parser.YaaiLexer) error {
+	opts := &snapshotOpts{
+		snapshotDir: os.Getenv("YAAI_SNAPSHOT_DIR"),
+		generate:    os.Getenv("YAAI_GENERATE") != "",
+	}
+
+	tl := &testListener{}
+	lexer.AddErrorListener(tl)
+
+	generate := func() ([]byte, error) {
+		buf := bytes.NewBuffer([]byte{})
+		for {
+			t := lexer.NextToken()
+			if t.GetTokenType() == antlr.TokenEOF {
+				buf.WriteString("EOF\n")
+				break
+			}
+			buf.WriteString(fmt.Sprintf("%s(%s) -> ",
+				lexer.SymbolicNames[t.GetTokenType()],
+				t.GetText()))
+		}
+
+		if len(tl.errors) > 0 {
+			// If we have lexer errors, simply return the first
+			return nil, tl.errors[0]
+		}
+		return buf.Bytes(), nil
+	}
+
+	return snapshot(name, generate, opts)
+}
+
+func snapshot(name string, generate func() ([]byte, error), options *snapshotOpts) error {
+	if options == nil {
+		return fmt.Errorf("provided options are nil")
+	}
+
+	snapshotFilename := fmt.Sprintf("%s.snapshot", name)
+	path := path.Join(options.snapshotDir, snapshotFilename)
+
+	// generate the data that will either be used to write or test against an existing
+	// snapshot file.
+	data, err := generate()
+	if err != nil {
+		return fmt.Errorf("could not generate data to compare/write: %v", err)
+	}
+
+	if options.generate {
+		return snapshotWrite(path, data)
+	} else {
+		return snapshotDiff(path, data)
+	}
+
+}
+
+func snapshotWrite(path string, data []byte) error {
+	err := os.WriteFile(path, data, 0644)
+	if err != nil {
+		return fmt.Errorf("Issue writing snapshot to path %s: %v", path, err)
+	}
+	return nil
+}
+
+func snapshotDiff(path string, data []byte) error {
+	// check if the file exists
+	fInfo, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("snapshot file not found %s: %v", path, err)
+	}
+
+	if fInfo.IsDir() {
+		return fmt.Errorf("snapshot path points to directory: %s", path)
+	}
+
+	// read in the snapshot file
+	snapshot, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("could not read snapshot file %s: %v", path, err)
+	}
+
+	// compare the daters
+	if bytes.Compare(snapshot, data) != 0 {
+		printDiff(snapshot, data)
+
+	}
+
+	return nil
+}
+
+// Print the diff between two strings
+func printDiff(a, b []byte) {
+	diff := diffmatchpatch.New()
+	diffs := diff.DiffMain(string(a), string(b), true)
+
+	fmt.Println(diff.DiffPrettyText(diffs))
+}
